abc208d16c6cee47235bea2546c6abebf2d64ee7
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-12 22:22:00 +0200

    Added basic js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("hi");
+const handSeconds = document.querySelector(".clock__hand--seconds");
+
+const setDate = () => {
+  console.log("hi");
+  const degrees = 110;
+  handSeconds.style.transform = `rotate(${degrees}deg)`;
+};
+setInterval(setDate, 1000);
